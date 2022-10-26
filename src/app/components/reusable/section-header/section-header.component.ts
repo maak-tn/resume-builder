ef92ff36d1eba413d13be56654ea1617e11913bd
aymkh <aymen4real@gmail.com>
2022-10-26 09:31:00 +0100

    section header reusable component

--- src/app/components/reusable/section-header/section-header.component.ts
+++ src/app/components/reusable/section-header/section-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
+  @Input()
+  headerTitle: any;
 
   constructor() { }
 
